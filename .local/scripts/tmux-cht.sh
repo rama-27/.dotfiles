4e348259d404228c9be8f2c2bab254d40c037f9e
rama-27 <ramarajumantena231@gmail.com>
2025-02-26 15:40:06 +0530

    customized tmux config

--- .local/scripts/tmux-cht.sh
+++ .local/scripts/tmux-cht.sh
@@ -6,9 +6,9 @@ fi
 
 read -p "Enter Query: " query
 
-if grep -qs "$selected" ~/.tmux-cht-languages; then
+if grep -qs "$selected" ~/.local/scripts/.tmux-cht-languages; then
     query=`echo $query | tr ' ' '+'`
-    tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
+    tmux neww bash -c "echo \"curl cheat.sh/$selected/$query/\" & curl cheat.sh/$selected/:$query & while [ : ]; do sleep 1; done"
 else
     tmux neww bash -c "curl -s cht.sh/$selected~$query | less -r"
 fi
